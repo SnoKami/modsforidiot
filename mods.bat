d3cf2314c48256f1a5f2ff159b870c8d28ac8d6a
aga <103972491+SnoKami@users.noreply.github.com>
2022-08-14 15:41:41 -0400

    Add files via upload

---
+++ mods.bat
@@ -0,0 +1,16 @@
+cd %APPDATA%\.minecraft\mods
+mkdir modsthatarealreadyinfolder
+mv * ./modsthatarealreadyinfolder
+
+curl https://github.com/SnoKami/modsforidiot/raw/main/AdvancedCompass-fabric-1.18.2-1.2.11.jar -O %APPDATA%\.minecraft\mods\AdvancedCompass-fabric-1.18.2-1.2.11.jar
+curl https://github.com/SnoKami/modsforidiot/raw/main/AdvancementPlaques-1.18.2-fabric-1.4.5.jar -O %APPDATA%\.minecraft\mods\AdvancementPlaques-1.18.2-fabric-1.4.5.jar
+curl https://github.com/SnoKami/modsforidiot/raw/main/ForgeConfigAPIPort-v3.2.0-1.18.2-Fabric.jar -O %APPDATA%\.minecraft\mods\ForgeConfigAPIPort-v3.2.0-1.18.2-Fabric.jar
+curl https://github.com/SnoKami/modsforidiot/raw/main/Iceberg-1.18.2-1.0.40.jar -O %APPDATA%\.minecraft\mods\Iceberg-1.18.2-1.0.40.jar
+curl https://github.com/SnoKami/modsforidiot/raw/main/balm-fabric-3.2.0%2B0.jar -O %APPDATA%\.minecraft\mods\balm-fabric-3.2.0%2B0.jar
+curl https://github.com/SnoKami/modsforidiot/raw/main/cloth-config-6.2.57-fabric.jar -O %APPDATA%\.minecraft\mods\cloth-config-6.2.57-fabric.jar
+curl https://github.com/SnoKami/modsforidiot/raw/main/comforts-fabric-1.1.2-1.18.2.jar -O %APPDATA%\.minecraft\mods\comforts-fabric-1.1.2-1.18.2.jar
+curl https://github.com/SnoKami/modsforidiot/raw/main/dual_riders-1.18.2-1.1.1-fabric.jar -O %APPDATA%\.minecraft\mods\dual_riders-1.18.2-1.1.1-fabric.jar
+curl https://github.com/SnoKami/modsforidiot/raw/main/fabric-api-0.58.0%2B1.18.2.jar -O %APPDATA%\.minecraft\mods\fabric-api-0.58.0%2B1.18.2.jar
+curl https://github.com/SnoKami/modsforidiot/raw/main/flytre_lib-1.18.2-1.8.1-fabric.jar -O %APPDATA%\.minecraft\mods\flytre_lib-1.18.2-1.8.1-fabric.jar
+curl https://github.com/SnoKami/modsforidiot/raw/main/medievalweapons-1.2.0.jar -O %APPDATA%\.minecraft\mods\medievalweapons-1.2.0.jar
+curl https://github.com/SnoKami/modsforidiot/raw/main/waystones-fabric-1.18.2-10.1.0.jar -O %APPDATA%\.minecraft\mods\waystones-fabric-1.18.2-10.1.0.jar
